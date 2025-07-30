990550074cc96bcf1c8491261d37c7ebd2b5037f
Tammineni Venkat Saketh <24PA1A05M4@VISHNU.EDU.IN>
2025-07-30 12:34:04 +0530

    Add files via upload

---
+++ PersonDemo.java
@@ -0,0 +1,20 @@
+//Invoking class and method in class
+class Person{
+	int age;
+	String name;
+	void setDetails(int a,String b){
+		age=a;
+		name=b;
+	}
+	void getDetails(){
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+	}	
+}
+public class PersonDemo{
+	public static void main(String args[]){
+		Person p1=new Person();
+		p1.setDetails(4,"VenkatSaketh");
+		p1.getDetails();
+	}
+}		
---
+++ RectangleDemo.java
@@ -0,0 +1,26 @@
+//Rectangle properties
+class Rectangle{
+	double length;
+	double width;
+	public Rectangle(){
+		this.length=0.0;
+		this.width=0.0;
+	}
+	public Rectangle(double l,double w){
+		this.length=l;
+		this.width=w;
+	}
+	public void getArea(double length,double width){
+		System.out.println("Area of Rectangle= "+length*width);
+	}
+	public void getPerimeter(double length,double width){
+		System.out.println("Perimeter of Rectangle= "+2*(length+width));
+	}
+}
+public class RectangleDemo{
+	public static void main(String args[]){
+		Rectangle r1=new Rectangle(10,20);
+		r1.getArea(10,20);
+		r1.getPerimeter(10,20);
+	}
+}			
